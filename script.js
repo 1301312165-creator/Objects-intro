89a7fc2ed36a79cfdc08c0ba3856f8ca19a3230c
1301312165-creator <1301312165@s.tusd1.org>
2026-04-15 17:45:27 +0000

    cookedfn

--- script.js
+++ script.js
@@ -31,26 +31,32 @@ function processForm(event) {
         favoriteColor: document.getElementById("favcolor").value
     }
     console.log(newUser);
+
+    addUser(newUser);
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
     // TODO: Set the background color of the div to the user's favorite color
+    div.style.backgroundColor = user.favoriteColor;
 
     // Create a heading for the user's name
     let name = document.createElement("h2");
     // TODO: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
     let email = document.createElement("p");
     // TODO: Set the text using the object data
+    email.innerText = user.email;
     div.appendChild(email); // Add the paragraph to the div
-    
-    // Create a paragrapn for the user's birthdate
+
+    // Create a paragraph for the user's birthdate
     let birthdate = document.createElement("p");
     // TODO: Set the text using the object data
+    birthdate.innerText = user.birthdate;
     div.appendChild(birthdate); // Add the paragraph to the div
 
     document.body.appendChild(div); // Add the div to the page
